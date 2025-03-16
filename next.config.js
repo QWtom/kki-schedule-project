1b66351e529bda3357319180d6b70b2bf50cd9ce
Sviatoslav Tomsky <qw.tom2@gmail.com>
2025-03-16 20:40:21 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
   images: {
     unoptimized: true
   },
-  experimental: {
-    serverComponentsExternalPackages: ['googleapis']
-  }
+  transpilePackages: []
 };
 
 module.exports = nextConfig;
