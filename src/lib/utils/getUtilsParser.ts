69967245ddc82dc6d9058be5e0458d3b69106b4d
Sviatoslav Tomsky <qw.tom2@gmail.com>
2025-03-31 19:48:27 +0900

    Update getUtilsParser.ts

--- src/lib/utils/getUtilsParser.ts
+++ src/lib/utils/getUtilsParser.ts
@@ -26,36 +26,4 @@ const getCurrentDayId = (): string => {
 	return String(today === 0 ? 6 : today);
 };
 
-// const sortLessonsByTime = (lessons: Lesson[]): Lesson[] => {
-// 	return [...lessons].sort((a, b) => {
-// 		const timeA = a.timeSlot.start.replace(':', '');
-// 		const timeB = b.timeSlot.start.replace(':', '');
-// 		return Number(timeA) - Number(timeB);
-// 	});
-// };
-
-// const getWeekSchedule = (
-// 	parsedSchedule: ParsedSchedule,
-// 	groupId: string
-// ): Record<string, Lesson[]> => {
-// 	const weekSchedule: Record<string, Lesson[]> = {};
-
-// 	DAYS_OF_WEEK.forEach(day => {
-// 		weekSchedule[day.id] = sortLessonsByTime(
-// 			parsedSchedule.schedule[groupId]?.[day.name] || []
-// 		);
-// 	});
-
-// 	return weekSchedule;
-// };
-
-// const hasLessonsOnDay = (
-// 	parsedSchedule: ParsedSchedule,
-// 	groupId: string,
-// 	dayId: string
-// ): boolean => {
-// 	const lessons = getGroupDaySchedule(parsedSchedule, groupId, dayId);
-// 	return lessons.length > 0;
-// };
-
 export { getGroupDaySchedule, getCurrentDayId }
